a03135a9b18f9db295330afc6a51740e11ff98af
Ross van der Heyde <rossvdheyde@gmail.com>
2018-06-27 12:32:40 +0200

    Implemented basics of ball maze with gyroscope

--- BallMaze/ballMaze.py
+++ BallMaze/ballMaze.py
@@ -8,103 +8,102 @@ from sense_hat import ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
 import time
 from signal import pause
 
-def pushed_up(event):
-    if event.action == ACTION_RELEASED:
-        sense.set_pixel(marker[0], marker[1], maze[8*marker[1] + marker[0]])
-        marker[1] = marker[1] +1
-        
-        #test for type of move
-        move = getMoveType(marker)
-        if move == LEGAL:
-            sense.set_pixel(marker[0], marker[1], ora)
-        elif move == WALL:
-            marker[1] = marker[1] - 1
-            sense.set_pixel(marker[0], marker[1], ora)
-        elif move == DIE:
-            die()
-        else:
-            win()
+def moveBallUp():
+    sense.set_pixel(ball[0], ball[1], [0,0,0])
+    ball[1] = ball[1] - 1
+    
+    #test for type of move
+    move = getMoveType(ball)
+    if move == LEGAL:
+        sense.set_pixel(ball[0], ball[1], ora)
+    elif move == DIE:
+        die()
+    elif move == WALL:
+        ball[1] = ball[1] + 1
+        sense.set_pixel(ball[0], ball[1], ora)
+    else:
+        win()
 
         
-def pushed_down(event):
-    if event.action == ACTION_RELEASED:
-        sense.set_pixel(marker[0], marker[1], maze[8*marker[1] + marker[0]])
-        marker[1] = marker[1] - 1
-        #test for type of move
-        move = getMoveType(marker)
-        if move == LEGAL:
-            sense.set_pixel(marker[0], marker[1], ora)
-        elif move == WALL:
-            marker[1] = marker[1] + 1
-            sense.set_pixel(marker[0], marker[1], ora)
-        elif move == DIE:
-            die()
-        else:
-            win()
+def moveBallDown():
+    sense.set_pixel(ball[0], ball[1], [0,0,0])
+    ball[1] = ball[1] + 1
 
-def pushed_right(event):
-    if event.action == ACTION_RELEASED:
-        sense.set_pixel(marker[0], marker[1], maze[8*marker[1] + marker[0]])
-        marker[0] = marker[0] - 1
-        
-        #test for type of move
-        move = getMoveType(marker)
-        if move == LEGAL:
-            sense.set_pixel(marker[0], marker[1], ora)
-        elif move == WALL:
-            marker[0] = marker[0] + 1
-            sense.set_pixel(marker[0], marker[1], ora)
-        elif move == DIE:
-            die()
-        else:
-            win()
+    #test for type of move
+    move = getMoveType(ball)
+    if move == LEGAL:
+        sense.set_pixel(ball[0], ball[1], ora)
+    elif move == DIE:
+        die()
+    elif move == WALL:
+        ball[1] = ball[1] - 1
+        sense.set_pixel(ball[0], ball[1], ora)
+    else:
+        win()
 
-def pushed_left(event):
-    if event.action == ACTION_RELEASED:
-        sense.set_pixel(marker[0], marker[1], maze[8*marker[1] + marker[0]])
-        marker[0] = marker[0] + 1
+def moveBallRight():
+    sense.set_pixel(ball[0], ball[1], [0,0,0])
+    ball[0] = ball[0] + 1
 
-        #test for type of move
-        move = getMoveType(marker)
-        if move == LEGAL:
-            sense.set_pixel(marker[0], marker[1], ora)
-        elif move == WALL:
-            marker[0] = marker[0] - 1
-            sense.set_pixel(marker[0], marker[1], ora)
-        elif move == DIE:
-            die()
-        else:
-            win()
+    #test for type of move
+    move = getMoveType(ball)
+    if move == LEGAL:
+        sense.set_pixel(ball[0], ball[1], ora)
+    elif move == WALL:
+        ball[0] = ball[0] - 1
+        sense.set_pixel(ball[0], ball[1], ora)
+    elif move == DIE:
+        die()
+    else:
+        win()
 
-def buttonPushed(event):
-    pass
+def moveBallLeft():
+    sense.set_pixel(ball[0], ball[1], [0,0,0])
+    ball[0] = ball[0] - 1
+    
+    #test for type of move
+    move = getMoveType(ball)
+    if move == LEGAL:
+        sense.set_pixel(ball[0], ball[1], ora)
+    elif move == WALL:
+        ball[0] = ball[0] + 1
+        sense.set_pixel(ball[0], ball[1], ora)
+    elif move == DIE:
+        die()
+    else:
+        win()
 
-def getMoveType(marker):
-    if not(0<= marker[0] <=7 and 0<= marker[1] <=7):
+
+def getMoveType(ball):
+    if not(0<= ball[0] <=7 and 0<= ball[1] <=7):
         return DIE #out of bounds move. you die
-    elif maze[8*marker[1] + marker[0]] == blu:
+    elif maze[8*ball[1] + ball[0]] == blu:
         return WALL #move into wall. you can't move
-    elif marker == end:
+    elif ball == end:
         return WIN #win
     else:
         return LEGAL #normal legal move
 
 def die():
+    global keepPlaying
     time.sleep(0.5)
     sense.show_message("You died")
     #restart
-    marker = start
-    sense.set_pixels(maze)
-    sense.set_pixel(marker[0], marker[1], ora)
+##    ball = start
+##    sense.set_pixels(maze)
+##    sense.set_pixel(ball[0], ball[1], ora)
+    keepPlaying = False
 
 def win():
+    global keepPlaying
     sense.set_pixel(end[0], end[1], ora)
     time.sleep(0.5)
     sense.show_message("You win")
     #restart
-    marker = start
-    sense.set_pixels(maze)
-    sense.set_pixel(marker[0], marker[1], ora)
+##    ball = start
+##    sense.set_pixels(maze)
+##    sense.set_pixel(ball[0], ball[1], ora)
+    keepPlaying = False
     
 # define moves
 DIE = -1
@@ -134,32 +133,48 @@ maze = [blk, blk, blk, blk, blu, blk, blk, gre,
 sense = sense_hat.SenseHat()
 sense.low_light = True
 sense.set_rotation(180)
-sense.set_imu_config(False, True, False)  # gyro only
-
-sense.stick.direction_up = pushed_up
-sense.stick.direction_down = pushed_down
-sense.stick.direction_left = pushed_left
-sense.stick.direction_right = pushed_right
-sense.stick.direction_middle = buttonPushed
+sense.set_imu_config(True, True, True)
 
 sense.set_pixels(maze)
 
 start = [0,7]
 end = [7,0]
-marker = start
-sense.set_pixel(marker[0], marker[1], ora)
+ball = start
+sense.set_pixel(ball[0], ball[1], ora)
 
-print("paused")
-pause()
+gyro = sense.get_orientation_degrees()
+prevPitch = gyro["pitch"]
+pitch = gyro["pitch"]
 
-      
-##while True:
-##    time.sleep(0.5)
-##    accel = sense.get_orientation_degrees()
-##    print("pitch: ", accel["pitch"])
-##    #print("roll: ", accel["roll"])
+prevRoll = gyro["roll"]
+roll = gyro["roll"]
 
+keepPlaying = True
 
+while keepPlaying:
+    time.sleep(0.1)
+    gyro = sense.get_orientation_degrees()
+    prevPitch = pitch
+    pitch = gyro["pitch"]
+
+    if pitch - prevPitch < -2:
+        #move ball one way
+        moveBallLeft()
+    elif pitch - prevPitch > 2:
+        #move ball the other way
+        moveBallRight()
+
+    if keepPlaying:
+        prevRoll = roll
+        roll = gyro["roll"]
+        if roll - prevRoll < -2:
+            #move ball one way
+            moveBallDown()
+        elif roll - prevRoll > 2:
+            #move ball the other way
+            moveBallUp()
+
+print("end")
 
 
 
