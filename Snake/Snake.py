90aa9b025d888bf12deec92d36d246efc7b3ef1e
Ross van der Heyde <rossvdheyde@gmail.com>
2018-07-06 10:57:39 +0200

    Minor refactoring in Snake.py

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -10,7 +10,7 @@ from signal import pause
 import random
 
 #SORT OUT THESE NAMES!
-def down(event):
+def south(event):
     global DIRECTION, turningPoints, snake
     if event.action == ACTION_RELEASED:
         if DIRECTION != DOWN: # don't let the snake go back on itself
@@ -19,21 +19,21 @@ def down(event):
             #dictionary key
             turningPoints[tuple(snake[0][0])] = UP
 
-def up(event):
+def north(event):
     global DIRECTION, turningPoints, snake
     if event.action == ACTION_RELEASED:
         if DIRECTION != UP:
             DIRECTION = DOWN
             turningPoints[tuple(snake[0][0])] = DOWN
         
-def left(event):
+def west(event):
     global DIRECTION, turningPoints, snake
     if event.action == ACTION_RELEASED:
         if DIRECTION != LEFT:
             DIRECTION = RIGHT
             turningPoints[tuple(snake[0][0])] = RIGHT
 
-def right(event):
+def east(event):
     global DIRECTION, turningPoints, snake
     if event.action == ACTION_RELEASED:
         if DIRECTION != RIGHT:
@@ -50,14 +50,15 @@ def slither():
     """Moves the snake and food's co-ordinates, but does not set the LEDs
     in the matrix."""
     global food, alive
-    grow = []
-    eat = False
+    newSegment = []
+
     for i in range(len(snake)):
         segment = snake[i]
         #convert to tuple because a list can't be used as a
         #dictionary key
         pixel = tuple(segment[0])
 
+        # check for turning point
         if pixel in turningPoints.keys():
             segment[1] = turningPoints[pixel]
             
@@ -68,17 +69,15 @@ def slither():
 
         if i == 0 and pixel == tuple(food):
             # the head eats the food
-            print("eat")
-
-            #snake grows (new segment to be added)
-            grow = list(snake[len(snake)-1])
-            grow[0] = list(grow[0])
-            snake.append(grow)
+            # snake grows (new segment to be added)
+            newSegment = list(snake[len(snake)-1])
+            newSegment[0] = list(newSegment[0])
+            snake.append(newSegment)
             
             #move food
             food = generateFood()
 
-        #move
+        #move the snake
         dire = segment[1]
         if dire == UP:
             segment[0][1] -=1
@@ -89,7 +88,7 @@ def slither():
         else:
             segment[0][0] +=1
 
-        #check if new position is not in the snake
+        #check if new position is in the snake
         snakePixels = []
         for i in snake:
             snakePixels.append(i[0])
@@ -97,10 +96,9 @@ def slither():
         if snakePixels.count(segment[0]) > 1:
             die()
             return
-        
 
 def generateFood():
-    """Place sthe food (target LED) on a random LED in the matrix
+    """Places the food (target LED) on a random LED in the matrix
     that is not in the snake"""
     temp = [random.randint(0,7), random.randint(0,7)]
 
@@ -114,30 +112,31 @@ def generateFood():
 
     return temp
 
-def play():
+def updateMatrix():
+    """Draws the snake and food in the new positions on the LED matrix"""
     global alive
-    # draw snake
+
+    # draw food
     sense.clear()
     sense.set_pixel(food[0], food[1], gre)
-    
-    for dot in snake:
-        if 0 <= dot[0][0] <= 7 and 0 <= dot[0][1] <= 7:
-            sense.set_pixel(dot[0][0], dot[0][1], blu)
+
+    # draw snake
+    for pixel in snake:
+        if 0 <= pixel[0][0] <= 7 and 0 <= pixel[0][1] <= 7:
+            sense.set_pixel(pixel[0][0], pixel[0][1], blu)
         else:
             die()
             return
 
-    if alive:
-        # blink snake's head
-        for i in range(2):
-            x = snake[0][0][0]
-            y = snake[0][0][1]
-            sense.set_pixel(x, y, blk)
-            time.sleep(0.2)
-            sense.set_pixel(x, y, blu)
-            time.sleep(0.2)
+    # blink snake's head
+    for i in range(2):
+        x = snake[0][0][0]
+        y = snake[0][0][1]
+        sense.set_pixel(x, y, blk)
+        time.sleep(0.2)
+        sense.set_pixel(x, y, blu)
+        time.sleep(0.2)
 
-        slither()
 
 def die():
     """When the snake dies by going off the grid or into itself"""
@@ -147,15 +146,15 @@ def die():
     sense.show_message("Final length: "+ str(len(snake)))
 
 #MAIN
+#set up the senseHat stuff
 sense = sense_hat.SenseHat()
 
 sense.low_light = True
-sense.set_rotation(180)
-
-sense.stick.direction_down = down
-sense.stick.direction_up = up
-sense.stick.direction_left = left
-sense.stick.direction_right = right
+##sense.set_rotation(180)
+sense.stick.direction_down = south
+sense.stick.direction_up = north
+sense.stick.direction_left = west
+sense.stick.direction_right = east
 sense.stick.direction_middle = stopGame
 
 blu = [0,0,255]
@@ -166,6 +165,7 @@ ora = [255,200,0]
 yel = [255,255,0]
 blk = [0,0,0]
 
+# direction constants
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -182,18 +182,14 @@ while playAgain:
     # the snake turns as the value, e.g. {(3, 5): 1} (where 1 is RIGHT)
     turningPoints = {}
 
-    # the snake is a list of pixels, where a pixel is a 2-tuple where
-    # the first entry is a list with the co-ords, and the second is the
-    # direction that the pixel is moving in.
-    snake = [[[4,1], DOWN], [[4,0], DOWN]]
-
+    # the snake is a list of segments, where a segment is a list where
+    # the first entry is a list with the co-ords of the pixel,
+    # and the second is the direction that the pixel is moving in.
     #we are reaching the point where Snake probably needs to be a class
     #or maybe just pixel, and then snake is a list of pixels
+    snake = [[[4,1], DOWN], [[4,0], DOWN]]    
 
-    ##food = generateFood()
-
-    food = [4,3]
-    ##sense.set_pixel(food[0], food[1], gre)
+    food = generateFood()
 
     alive = True
 
@@ -207,7 +203,8 @@ while playAgain:
 
     #start game play
     while alive:
-        play()
+        updateMatrix()
+        slither()
 
 
 sense.clear()
