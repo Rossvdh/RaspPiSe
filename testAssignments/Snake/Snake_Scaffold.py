f98fc0658971189bb00b79663ebd6ab238d1f159
Ross van der Heyde <rossvdheyde@gmail.com>
2018-08-19 16:07:15 +0200

    Wrote test assignment for snake

--- testAssignments/Snake/Snake_Scaffold.py
+++ testAssignments/Snake/Snake_Scaffold.py
@@ -6,6 +6,20 @@ import time
 import random
 import SnakeClass
 
+# define some colours
+blu = [0,0,255]
+red = [255,0,0]
+gre = [0,255,0]
+whi = [255,255,255]
+ora = [255,200,0]
+yel = [255,255,0]
+blk = [0,0,0]
+
+# direction constants
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
 
 def up(event):
     """When the joystick is pushed up, change the snake's direction
@@ -28,13 +42,15 @@ def right(event):
 def left(event):
     """When the joystick is pushed left, change the snake's direction
     of movement to the left"""
-    # Task 2: implement this function
-    pass
+    if event.action == sense_hat.ACTION_RELEASED:
+        snake.changeDirection(LEFT)
 
 def stopGame(event):
     """When the middle joystick button is pressed, stop the game"""
-    # Task 5: implement this function
-    pass
+    if event.action == sense_hat.ACTION_RELEASED:
+        global playAgain, alive
+        playAgain = False
+        alive = False
 
 def generateFood():
     """Places the food (target LED) on a random LED in the matrix
@@ -44,13 +60,34 @@ def generateFood():
 
 def updateMatrix(food, snake):
     """Draws the snake and food in the new positions on the LED matrix"""
-    #pass
-    # Task 1: display the foodand the snake on the LED matrix
+    sense.clear()
+    # Task 1: display the food on the LED matrix
+    
+    # draw snake
+    snakePixels = snake.getPixels()
+    for pixel in snakePixels:
+        if 0 <= pixel[0] <= 7 and 0 <= pixel[1] <= 7:
+            sense.set_pixel(pixel[0], pixel[1], blu)
+        else:
+            die()
+            return
+
+    # blink snake's head
+    for i in range(2):
+        x = snake.head()[0]
+        y = snake.head()[1]
+        sense.set_pixel(x, y, blk)
+        time.sleep(0.1)
+        sense.set_pixel(x, y, blu)
+        time.sleep(0.1)
+    
 
 def die():
     """When the snake dies by going off the grid or into itself"""
-    # Task 4: implement this function
-    pass
+    global alive
+    alive = False
+    sense.show_message("You died", text_colour=[255,51,0])
+    sense.show_message("Final length: "+ str(snake.getLength()))
     
 
 # MAIN ----------------------------------------------------------------
@@ -62,19 +99,6 @@ sense.stick.direction_up = up
 sense.stick.direction_left = left
 sense.stick.direction_right = right
 sense.stick.direction_middle = stopGame
-blu = [0,0,255]
-red = [255,0,0]
-gre = [0,255,0]
-whi = [255,255,255]
-ora = [255,200,0]
-yel = [255,255,0]
-blk = [0,0,0]
-
-# direction constants
-UP = 0
-RIGHT = 1
-DOWN = 2
-LEFT = 3
 
 playAgain = True
 
